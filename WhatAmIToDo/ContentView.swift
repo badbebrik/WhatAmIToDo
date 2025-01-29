feab2c8773a73a1f44c5c57eb394214505a3eb2d
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-01-29 14:47:03 +0300

    Initial Commit

---
+++ WhatAmIToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatAmIToDo
+//
+//  Created by Виктория Серикова on 29.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatAmIToDo/WhatAmIToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatAmIToDoApp.swift
+//  WhatAmIToDo
+//
+//  Created by Виктория Серикова on 29.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatAmIToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

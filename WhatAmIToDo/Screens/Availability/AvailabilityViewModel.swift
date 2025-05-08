dccb28c209342e3be13b7a9730decf6f9445baaa
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-05-08 19:34:20 +0300

    Implement AvailabilityViewModel

--- WhatAmIToDo/Screens/Availability/AvailabilityViewModel.swift
+++ WhatAmIToDo/Screens/Availability/AvailabilityViewModel.swift
@@ -6,8 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
 
 @MainActor
-class AvailabilityViewModel: ObservableObject {
-    
+final class AvailabilityViewModel: ObservableObject {
+    @Published var days: [DayAvailabilityItem] = DayOfWeek.allCases.map {
+        .init(day: $0, slots: [])
+    }
+
+    private let goalId: UUID
+    private let network: GoalNetworkManager = .shared
+
+    init(goalId: UUID) {
+        self.goalId = goalId
+    }
+
+    func slots(for day: DayOfWeek) -> [TimeSlotItem] {
+        days.first { $0.day == day }?.slots ?? []
+    }
+
+    func setSlots(_ newSlots: [TimeSlotItem], for day: DayOfWeek) {
+        guard let idx = days.firstIndex(where: { $0.day == day }) else {
+            return
+        }
+        days[idx].slots = newSlots.sorted()
+    }
+
 }

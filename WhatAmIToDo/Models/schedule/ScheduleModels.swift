5dfb6e39a1fa65ef6c02a04057238af606241149
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-05-10 01:13:46 +0300

    add ScheduledTaskItem model

---
+++ WhatAmIToDo/Models/schedule/ScheduleModels.swift
@@ -0,0 +1,33 @@
+//
+//  ScheduleModels.swift
+//  WhatAmIToDo
+//
+//  Created by Виктория Серикова on 10.05.2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct ScheduledTaskItem: Identifiable {
+    let id: UUID
+    let title: String
+    let start: Date
+    let end:   Date
+    let status: TaskStatus
+
+    init(dto: ScheduledTaskDTO) {
+        self.id     = dto.id
+        self.title  = dto.title
+        self.start  = dto.startTime.dateHM
+        self.end    = dto.endTime.dateHM
+        self.status = TaskStatus(rawValue: dto.status) ?? .pending
+    }
+}
+
+private extension String {
+    var dateHM: Date {
+        let df = DateFormatter()
+        df.dateFormat = "HH:mm"
+        return df.date(from: self) ?? .init()
+    }
+}

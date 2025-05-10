fadca97c5eea9d689093469e246d05198a555faa
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-05-10 06:25:22 +0300

    Add DashboardView

---
+++ WhatAmIToDo/Screens/Dashboard/DashboardView.swift
@@ -0,0 +1,42 @@
+//
+//  DashboardView.swift
+//  WhatAmIToDo
+//
+//  Created by Виктория Серикова on 10.05.2025.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+
+    @StateObject var viewModel: DashboardViewModel
+    @Environment(\.colorScheme) private var scheme
+
+    var body: some View {
+        EmptyView()
+    }
+
+    private var greetingHeader: some View {
+
+    }
+
+    private struct ProgressRing: View {
+
+    }
+
+    private struct MotivationCard: View {
+
+    }
+
+    private struct UpcomingCarousel: View {
+
+    }
+
+    private struct TodaySection: View {
+
+    }
+
+    private struct StatsSection: View {
+        
+    }
+}

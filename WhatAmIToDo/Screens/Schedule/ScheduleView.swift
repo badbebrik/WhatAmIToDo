bfa2b21cf3f3d8281af868de3856af4284546fbf
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-05-10 03:00:04 +0300

    add range calendar, start inplement dateCarousel

--- WhatAmIToDo/Screens/Schedule/ScheduleView.swift
+++ WhatAmIToDo/Screens/Schedule/ScheduleView.swift
@@ -26,6 +26,16 @@ struct ScheduleView: View {
     }
 
     private struct DateCarousel: View {
+
+        @Binding var selected: Date
+        var onChange: (Date) -> Void
+
+        private let range: [Date] = {
+            let cal = Calendar.current
+            let start = cal.startOfDay(for: .now)
+            return (-7...7).compactMap { cal.date(byAdding: .day, value: $0, to: start)}
+        }()
+
         var body: some View {
             EmptyView()
         }
